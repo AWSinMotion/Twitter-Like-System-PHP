cd083187ed93eb4d289ab06053c78c5bb6e288c4
Michael Wittig <post@michaelwittig.info>
2016-12-12 12:57:03 +0100

    improved load testing page

--- loadtest.php
+++ loadtest.php
@@ -1,9 +1,18 @@
-<?php
-function fibonacci($n) {
-	if ($n == 0 || $n == 1) {
-		return $n;
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Loadtest</title>
+		<meta http-equiv="refresh" content="1">
+	</head>
+	<body>
+	<?php
+	function fibonacci($n) {
+		if ($n == 0 || $n == 1) {
+			return $n;
+		}
+		return fibonacci($n-2) + fibonacci($n-1);
 	}
-	return fibonacci($n-2) + fibonacci($n-1);
-}
-echo fibonacci(37);
-?>
+	echo fibonacci(35);
+	?>
+	</body>
+</html>
